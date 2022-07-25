901b9a324555fd0eda1ebdf1dc4db891b4f89165
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-25 16:40:45 +0500

    Create NewwaysOfDefiningFuncinObj.js

---
+++ NewwaysOfDefiningFuncinObj.js
@@ -0,0 +1,24 @@
+// This is the new way of defining functions inside of objects in javscript
+
+                      const myMath = {
+                        add(num1,num2){
+                            return num1 + num2
+                        },
+
+                        subract(num1,num2){
+                            return num1 - num2
+                        },
+
+                        multiply(num1,num2){
+                            return num1 * num2
+                        },
+
+                        divide(num1,num2){
+                            return num1/num2
+                        },
+
+                        square(num){
+                            return num**2
+                        }
+                      }
+               
