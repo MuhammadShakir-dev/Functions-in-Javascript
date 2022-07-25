ee37c000798d7ba6c64bed462ff3b61e7fc99c06
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-25 16:35:16 +0500

    Create DefiningMethodsInFunction.js

---
+++ DefiningMethodsInFunction.js
@@ -0,0 +1,37 @@
+/*
+
+WE ADD FUNCTION AS A PROPERTY OF ON OBJECTS.
+WE CALL THEM METHODS.
+
+*/
+
+
+   // The normal ways to do this.
+
+                  const myMath = {
+                    add : function(num1,num2){
+                        return num1 + num2
+                    },
+
+                    subract : function(num1,num2){
+                        return num1 - num2
+                    },
+
+                    multiply :function(num1,num2){
+                        return num1 * num2
+                    },
+
+                    divide : function(num1, num2){
+                        return num1/num2
+                    },
+
+                    square : function(num){
+                        return num*num
+                    },
+
+                    cube : function(num){
+                        return num**3
+                    }
+                  }
+                  
+    
