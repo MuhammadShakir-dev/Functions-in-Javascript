2481b80120794d60cfad85277711298146ff875d
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-26 13:15:58 +0500

    Update This.js

--- This.js
+++ This.js
@@ -1,1 +1,14 @@
 
+
+      const cat = {
+        name: "Mew",
+        Weight: "3kg",
+        Color: "White",
+        Sumup() {
+         return document.write(
+            this.name + "<br>" + this.Weight + "<br>" + this.Color
+          );
+        },
+      };
+
+      cat.Sumup();
