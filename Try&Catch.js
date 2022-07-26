8c9a39c8d5a558d94ae47809070ec19d92fa12d7
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-26 13:59:32 +0500

    Update Try&Catch.js

--- Try&Catch.js
+++ Try&Catch.js
@@ -1,1 +1,26 @@
 
+    /*
+
+                try and catch in javascript.
+
+                whats the difference between try and catch?
+                try:
+                    try to do something.
+                catch:
+                    catch the error.
+                finally:
+                    finally do something.
+
+
+
+
+        */
+                   function alert(msg){
+                    try{
+                        return(msg.toUpperCase().repeat(3));
+                    }catch (e){
+                        console.log (e);
+                        console.log("Please enter a string next time"); 
+                    }
+                   
+                   }
